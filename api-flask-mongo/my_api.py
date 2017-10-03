f6f10ab203f1694c2c4ea6b068c8d2701cfaec25
David Allen <bitlather@gmail.com>
2017-10-02 21:33:10 -0400

    Add delete endpoint

--- api-flask-mongo/my_api.py
+++ api-flask-mongo/my_api.py
@@ -1,10 +1,12 @@
 import json
 from bson import json_util
 from bson.objectid import ObjectId
-from flask import Flask
+from flask import Flask, request
+from flask.ext.api import status
 from flask_pymongo import PyMongo
+from flask_api import FlaskAPI
 
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 app.config['MONGO_DBNAME'] = 'tv'
 mongo = PyMongo(app, config_prefix='MONGO')
 
@@ -41,12 +43,26 @@ def seed():
 
 
 @app.route('/show') # GET /show
-def show():
+def GET_show():
     show = list(mongo.db.show.find())
     return json.dumps(show, default=json_util.default)
 
 
 @app.route('/show/<show_id>') # GET /show/{id}
-def show_id(show_id):
+def GET_show_id(show_id):
     show = list(mongo.db.show.find({ "_id" : str(show_id) }))
     return json.dumps(show, default=json_util.default)
+
+
+@app.route('/show/<show_id>', methods=['DELETE']) # DELETE /show/{id}
+def DELETE_show_id(show_id):
+    result = mongo.db.show.delete_one({ "_id" : str(show_id) })
+    if result.deleted_count == 1:
+        return '', status.HTTP_204_NO_CONTENT
+    else:
+        return {'error' : 'document not found'}, status.HTTP_404_NOT_FOUND
+
+
+@app.route('/show', methods=['POST'])
+def POST_show():
+    return 'done'
